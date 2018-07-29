aa9a5410ac37d53adcaa14df2d040edf65a3637b
Raul Vera <9060163+twyblade64@users.noreply.github.com>
2018-07-29 08:34:56 -0500

    Better grid stability

--- Assets/Scripts/ECS/Systems/MassSpringForceSystem.cs
+++ Assets/Scripts/ECS/Systems/MassSpringForceSystem.cs
@@ -20,7 +20,7 @@ public class MassSpringForceSystem : JobComponentSystem {
     public readonly int Length;
     [ReadOnly] public ComponentDataArray<EntityPair> EntityPairs;
     [ReadOnly] public ComponentDataArray<Elasticity> Elasticities;
-    [ReadOnly] public ComponentDataArray<Line> Lines;
+    [ReadOnly] public ComponentDataArray<Spring> Lines;
   }
 
   [Inject] private MassData _massData;
@@ -28,26 +28,42 @@ public class MassSpringForceSystem : JobComponentSystem {
 
   [BurstCompile]
   struct MassSpringForceJob : IJobParallelFor {
+    public int _massAmmount;
     public int _springAmmount;
     [ReadOnly] public ComponentDataArray<EntityPair> _springEntityPairs;
     [ReadOnly] public ComponentDataArray<Elasticity> _springElasticities;
-    [ReadOnly] public ComponentDataArray<Line> _springLines;
+    [ReadOnly] public ComponentDataArray<Spring> _springLines;
     [ReadOnly] public EntityArray _massEntities;
     public ComponentDataArray<Physical> _massPhysicals;
 
     public void Execute(int index) {
-      Entity massEntity = _massEntities[index];
-      float3 forceSum = new float3(0,0,0);
-      for (int i = 0; i < _springAmmount; ++i) {
-        if (massEntity == _springEntityPairs[i].E1) 
-          forceSum += (_springLines[i].p2 - _springLines[i].p1)*_springElasticities[i].Value;
-        if (massEntity == _springEntityPairs[i].E2)
-          forceSum += (_springLines[i].p1 - _springLines[i].p2)*_springElasticities[i].Value;
-      }
-      _massPhysicals[index] = new Physical { 
-        Force = _massPhysicals[index].Force + forceSum, 
-        InverseMass = _massPhysicals[index].InverseMass
-      };
+    //public void Execute() {
+      //for (int index = 0; index < _massAmmount; ++index) {
+        Entity massEntity = _massEntities[index];
+        float3 forceSum = new float3(0,0,0);
+        for (int i = 0; i < _springAmmount; ++i) {
+          if (massEntity == _springEntityPairs[i].E1) {
+            float3 dir = (_springLines[i].p2 - _springLines[i].p1);
+            float dist = math.length(dir);
+            float refDist = _springLines[i].length;
+            if (dist > refDist*refDist)
+              forceSum += dir/dist*math.min(dist-refDist,1)*_springElasticities[i].Value;
+          }
+          if (massEntity == _springEntityPairs[i].E2) {
+            float3 dir = (_springLines[i].p1 - _springLines[i].p2);
+            float dist = math.length(dir);
+            float refDist = _springLines[i].length;
+            if (dist > refDist*refDist)
+              forceSum += dir/dist*math.min(dist-refDist,1)*_springElasticities[i].Value;
+          }
+        }
+
+        //Debug.Log("ForceSum: "+forceSum);
+        _massPhysicals[index] = new Physical { 
+          Force = _massPhysicals[index].Force + forceSum, 
+          InverseMass = _massPhysicals[index].InverseMass
+        };
+      //}
     }
   }
 
@@ -56,6 +72,7 @@ public class MassSpringForceSystem : JobComponentSystem {
       return inputDeps;
 
     MassSpringForceJob job = new MassSpringForceJob {
+      //_massAmmount = _massData.Length,
       _springAmmount = _springData.Length,
       _springEntityPairs = _springData.EntityPairs,
       _springElasticities = _springData.Elasticities,
@@ -65,6 +82,7 @@ public class MassSpringForceSystem : JobComponentSystem {
     };
 
     JobHandle jobHandle = job.Schedule(_massData.Length, 64, inputDeps);
+    //JobHandle jobHandle = job.Schedule(inputDeps);
     return jobHandle;
   }
 }
