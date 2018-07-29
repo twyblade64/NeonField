7694152951d56833a47b115800238b517153aa8d
Raul Vera <9060163+twyblade64@users.noreply.github.com>
2018-07-29 14:17:28 -0500

    Added camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public float spaceRatio;
+    public float marginRatio;
+    public GameObject followObject;
+    private Vector3 startPosition;
+
+    private Camera m_camera;
+	// Use this for initialization
+	void Start () {
+        m_camera = gameObject.GetComponent<Camera>();
+        startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float ratioX = followObject.transform.position.x/(m_camera.orthographicSize * marginRatio * m_camera.aspect);
+        float ratioY = followObject.transform.position.z/(m_camera.orthographicSize * marginRatio);
+
+        ratioX = Mathf.Max(Mathf.Min(ratioX, 1), -1) * (spaceRatio-1);
+        ratioY = Mathf.Max(Mathf.Min(ratioY, 1), -1) * (spaceRatio-1);
+
+        Vector3 pos = startPosition;
+        pos.x += m_camera.orthographicSize * ratioX * m_camera.aspect;
+        pos.z += m_camera.orthographicSize * ratioY;
+        transform.position = pos;
+    }
+}
